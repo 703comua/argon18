f59256575f2512b4382fa0d5a5778a95d40a71ae
Sergey <52292140+703comua@users.noreply.github.com>
2024-03-15 21:33:20 +0200

    admin 15-03-2024

--- assets/admin/js/admin.js
+++ assets/admin/js/admin.js
@@ -8,6 +8,8 @@ $(document).ready(function () {
         closeExisting: true,
     });
 
+    $('.select2').select2();
+
     $(".nav__burger-btn").on("click", function () {
         $("body").toggleClass("sidebar-collapse");
         $(".dropdown-menu").removeClass("is-open");
@@ -26,11 +28,24 @@ $(document).ready(function () {
         // если список есть
         if ($(this).next("ul").length == 1) {
             $(this).next("ul").slideToggle();
-            $(this).parent("li").toggleClass("active");
+            // $(this).parent("li").toggleClass("active");
+            $(this).toggleClass("active");
         }
     });
     
-    
+    // breadcrumbs
+    const contentBreadcrumbsWrap = new Swiper(".content__breadcrumbs-wrap", {
+        slidesPerView: "auto",
+        freeMode: true, // при перетаскивании превью ведет себя как при скролле
+        grabCursor: true, // Если установлено значение true, указатель изменится на форму ладони, когда мышь накрывает Swiper, а указатель изменится на форму ручки при перетаскивании. (Зависит от формы браузера)
+        speed: 1000,
+        touchAngle: 10, // Allowable angle (in degrees) to trigger touch move
+        pagination: {
+            el: ".swiper-pagination",
+            type: "progressbar",
+        },
+    });
+
     // $(document).click(function (event) {
     //     // если мы кликнули в любом месте сайта, кроме иконки текущего выбранного языка
     //     if (!$(event.target).closest($(".фышву")).length) {
